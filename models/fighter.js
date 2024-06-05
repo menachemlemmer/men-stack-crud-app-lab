85e9a5ba4c0a3d8c9af6f48891d5fe4ae10d3cb9
menachemlemmer <nmmlemmer@gmail.com>
2024-06-05 11:56:03 -0400

    Complete men-stack-crud-app-lab

--- models/fighter.js
+++ models/fighter.js
@@ -1,12 +1,14 @@
 const mongoose = require("mongoose");
 
 const fighterSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   rank: Number,
-  division: String,
-  wins: Number,
-  losses: Number,
-  no_contest: Number,
+  division: { type: String, required: true },
+  wins: { type: Number, required: true },
+  losses: { type: Number, required: true },
+  no_contest: { type: Number, required: true },
+  isActive: { type: Boolean, required: true },
+  image: String,
 });
 
 const Fighter = mongoose.model("Fighter", fighterSchema);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const path = require("path");
 
 const app = express();
 
@@ -11,13 +14,51 @@ mongoose.connection.on("connected", () => {
 });
 
 const Fighter = require("./models/fighter");
+app.use(express.urlencoded({ extended: false }));
+app.use(morgan("dev"));
+app.use(methodOverride("_method"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
   res.render("index.ejs");
 });
 
+app.get("/fighters", async (req, res) => {
+  const allFighters = await Fighter.find({});
+  res.render("fighters/index.ejs", { fighters: allFighters });
+});
+
 app.get("/fighters/new", (req, res) => {
-  res.send("Create new fighter here");
+  res.render("fighters/new.ejs");
+});
+
+app.get("/fighters/:fighterId", async (req, res) => {
+  const foundFighter = await Fighter.findById(req.params.fighterId);
+  res.render("fighters/show.ejs", { fighter: foundFighter });
+});
+
+app.get("/fighters/:fighterId/edit", async (req, res) => {
+  const foundFighter = await Fighter.findById(req.params.fighterId);
+  res.render("fighters/edit.ejs", {
+    fighter: foundFighter,
+  });
+});
+
+app.post("/fighters", async (req, res) => {
+  req.body.isActive = !!req.body.isActive;
+  await Fighter.create(req.body);
+  res.redirect("/fighters");
+});
+
+app.delete("/fighters/:fighterId", async (req, res) => {
+  await Fighter.findByIdAndDelete(req.params.fighterId);
+  res.redirect("/fighters");
+});
+
+app.put("/fighters/:fighterId", async (req, res) => {
+  req.body.isActive = !!req.body.isActive;
+  await Fighter.findByIdAndUpdate(req.params.fighterId, req.body);
+  res.redirect(`/fighters/${req.params.fighterId}`);
 });
 
 app.listen(3000, () => {
